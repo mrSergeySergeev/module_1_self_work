3ab04db823ed27b28c64465558757ae46a72c20a
Sergey Sergeev <giiizmo270286@gmail.com>
2023-05-03 00:08:16 +0700

    counter-app

--- practice/counter-app/script.js
+++ practice/counter-app/script.js
@@ -1,20 +1,34 @@
-let addButton = document.querySelector('.js-add-button') ;
+let addButton = document.querySelector('.js-add-button');
 let resetButton = document.querySelector('.js-reset-button');
 let counterResult = document.querySelector('.js-counter');
 
 let counter = 80;
 
-addButton.addEventListener('click', function() {
+addButton.addEventListener('click', function () {
     counter = counter + 4;
-    console.log(counter)
+    console.log(counter);
     counterResult.innerText = counter;
-    return counter;
 });
 
-resetButton.addEventListener('click', function() {
+addButton.addEventListener('click', function() {
+    console.log(counter)
+});
+
+resetButton.addEventListener('click', function () {
     counter = 80;
 
     counterResult.innerText = counter;
 });
 
-if (counter >= 100) { } 
+const ACTIVE_IMAGE_CLASSNAME = 'gallery__image-active';
+const slidesNodes = Array.from(document.querySelectorAll('.gallery__image'));
+console.log(slidesNodes);
+
+if (counter >= 100 && counter <= 115) {
+    slidesNodes[0].classList.add(ACTIVE_IMAGE_CLASSNAME);
+}
+else
+    if (counter >= 116 && counter <= 121) {
+        slidesNodes[0].classList.remove(ACTIVE_IMAGE_CLASSNAME);
+        slidesNodes[1].classList.add(ACTIVE_IMAGE_CLASSNAME);
+    }
