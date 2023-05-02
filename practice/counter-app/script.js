751e4a4655d4727d2b1e447a36a61f54a2421fe8
Sergey Sergeev <giiizmo270286@gmail.com>
2023-05-03 00:23:26 +0700

    counter-app

--- practice/counter-app/script.js
+++ practice/counter-app/script.js
@@ -1,6 +1,10 @@
 let addButton = document.querySelector('.js-add-button');
 let resetButton = document.querySelector('.js-reset-button');
 let counterResult = document.querySelector('.js-counter');
+const ACTIVE_IMAGE_CLASSNAME = 'gallery__image-active';
+const slidesNodes = Array.from(document.querySelectorAll('.gallery__image'));
+console.log(slidesNodes);
+
 
 let counter = 80;
 
@@ -8,27 +12,52 @@ addButton.addEventListener('click', function () {
     counter = counter + 4;
     console.log(counter);
     counterResult.innerText = counter;
-});
-
-addButton.addEventListener('click', function() {
-    console.log(counter)
+    if (counter >= 100 && counter <= 115) {
+        slidesNodes[0].classList.add(ACTIVE_IMAGE_CLASSNAME);
+    }
+    else
+        if (counter >= 116 && counter <= 121) {
+            slidesNodes[0].classList.remove(ACTIVE_IMAGE_CLASSNAME);
+            slidesNodes[1].classList.add(ACTIVE_IMAGE_CLASSNAME);
+        }
+        else
+            if (counter >= 122 && counter <= 131) {
+                slidesNodes[1].classList.remove(ACTIVE_IMAGE_CLASSNAME);
+                slidesNodes[2].classList.add(ACTIVE_IMAGE_CLASSNAME);
+            }
+            else
+                if (counter >= 132 && counter <= 142) {
+                    slidesNodes[2].classList.remove(ACTIVE_IMAGE_CLASSNAME);
+                    slidesNodes[3].classList.add(ACTIVE_IMAGE_CLASSNAME);
+                }
+                else
+                    if (counter >= 143 && counter <= 157) {
+                        slidesNodes[3].classList.remove(ACTIVE_IMAGE_CLASSNAME);
+                        slidesNodes[4].classList.add(ACTIVE_IMAGE_CLASSNAME);
+                    }
+                    else
+                        if (counter >= 158 && counter <= 163) {
+                            slidesNodes[4].classList.remove(ACTIVE_IMAGE_CLASSNAME);
+                            slidesNodes[5].classList.add(ACTIVE_IMAGE_CLASSNAME);
+                        }
+                        else
+                            if (counter >= 164) {
+                                slidesNodes[5].classList.remove(ACTIVE_IMAGE_CLASSNAME);
+                                slidesNodes[6].classList.add(ACTIVE_IMAGE_CLASSNAME);
+                            }
 });
 
 resetButton.addEventListener('click', function () {
     counter = 80;
 
     counterResult.innerText = counter;
+    slidesNodes[0].classList.remove(ACTIVE_IMAGE_CLASSNAME);
+    slidesNodes[1].classList.remove(ACTIVE_IMAGE_CLASSNAME);
+    slidesNodes[2].classList.remove(ACTIVE_IMAGE_CLASSNAME);
+    slidesNodes[3].classList.remove(ACTIVE_IMAGE_CLASSNAME);
+    slidesNodes[4].classList.remove(ACTIVE_IMAGE_CLASSNAME);
+    slidesNodes[5].classList.remove(ACTIVE_IMAGE_CLASSNAME);
+    slidesNodes[6].classList.remove(ACTIVE_IMAGE_CLASSNAME);
 });
 
-const ACTIVE_IMAGE_CLASSNAME = 'gallery__image-active';
-const slidesNodes = Array.from(document.querySelectorAll('.gallery__image'));
-console.log(slidesNodes);
 
-if (counter >= 100 && counter <= 115) {
-    slidesNodes[0].classList.add(ACTIVE_IMAGE_CLASSNAME);
-}
-else
-    if (counter >= 116 && counter <= 121) {
-        slidesNodes[0].classList.remove(ACTIVE_IMAGE_CLASSNAME);
-        slidesNodes[1].classList.add(ACTIVE_IMAGE_CLASSNAME);
-    }
