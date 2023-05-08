f4ce3e85ef9c8e6bb9bc2a31ae0e8978246242eb
Sergey Sergeev <giiizmo270286@gmail.com>
2023-05-08 13:41:24 +0700

    counter-app

---
+++ practice/counter-app-two/script.js
@@ -0,0 +1,6 @@
+const counterValue = document.querySelector('[data-sum="sumValue]');
+const answerValue  = document.querySelector('[data-answer="answerValue"]');
+const addOneItemButton = document.getElementById("addOneItemButton");
+const resetButton = document.getElementById("resetButton");
+
+console.log(addOneItemButton.dataset);
