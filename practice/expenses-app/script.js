eb26f64325225251104699a1580272615fb5cbd6
Sergey Sergeev <giiizmo270286@gmail.com>
2023-05-09 10:59:44 +0700

    expenses-app

--- practice/expenses-app/script.js
+++ practice/expenses-app/script.js
@@ -1,2 +1,17 @@
 const expenses = [];
 
+const inputNode = document.getElementById('expenseInput');
+const buttonNode = document.getElementById('expenseButton');
+
+buttonNode.addEventListener('click', function() {
+    if (!inputNode.value) {
+        return;
+    }
+
+    const expense = parseInt(inputNode.value);
+
+    expenses.push(expense);
+
+    inputNode.value = '';
+    console.log(expenses);
+})
