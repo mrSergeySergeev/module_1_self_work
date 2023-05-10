8535a189d6b4f6c322db09ab50dc8deeacf9fccb
Sergey Sergeev <giiizmo270286@gmail.com>
2023-05-10 16:48:21 +0700

    expenses-app

--- practice/expenses-app/script.js
+++ practice/expenses-app/script.js
@@ -2,17 +2,30 @@ const expenses = [];
 
 const inputNode = document.getElementById('expenseInput');
 const buttonNode = document.getElementById('expenseButton');
+const historyNode = document.getElementById('expenseHistory');
 
-buttonNode.addEventListener('click', function() {
+buttonNode.addEventListener('click', function () {
     if (!inputNode.value) {
         return;
     }
-
+    //take value from input
     const expense = parseInt(inputNode.value);
 
+    //refresh input to zero
     inputNode.value = '';
 
+    //add value in array
     expenses.push(expense);
-    
+
+    //output new list 'expenses'
+    let expensionListHTML = '';
+
+    expenses.forEach(element => {
+        expensionListHTML += `<li>${element}</li>`;
+    });
+
+    historyNode.innerHTML = `<ol>${expensionListHTML}</ol>`;
+
+
     console.log(expenses);
 })
