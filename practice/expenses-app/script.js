2fa398804cf4a1af61aea5029140b98046c30542
Sergey Sergeev <giiizmo270286@gmail.com>
2023-05-12 13:18:35 +0700

    expenses-app

--- practice/expenses-app/script.js
+++ practice/expenses-app/script.js
@@ -1,6 +1,9 @@
 const LIMIT = 10000;
-
-const expenses = [];
+const CURRENCY = ' руб.';
+const STATUS_IN_LIMIT = 'всё хорошо';
+const STATUS_IN_LIMIT_CLASSNAME = 'status_good';
+const STATUS_OUT_OF_LIMIT = 'всё плохо';
+const STATUS_OUT_OF_LIMIT_CLASSNAME = 'status_bad';
 
 const inputNode = document.getElementById('expenseInput');
 const buttonNode = document.getElementById('expenseButton');
@@ -9,49 +12,86 @@ const sumNode = document.getElementById('expenseSum');
 const limitNode = document.getElementById('expenseLimit');
 const statusNode = document.getElementById('expenseStatus');
 
-limitNode.innerText = LIMIT;
+const expenses = [];
+
+init(expenses);
 
 buttonNode.addEventListener('click', function () {
-    if (!inputNode.value) {
+    const expanse = getExpanseFromUser();
+
+    if (!expanse) {
         return;
     }
-    //берём значение from input
+
+    trackExpanse(expanse);
+
+    render(expenses);
+});
+
+function init(expenses) {
+    limitNode.innerText = LIMIT;
+    statusNode.innerText = STATUS_IN_LIMIT;
+    sumNode.innerText = calculateExpanses(expenses);
+};
+
+function trackExpanse(expanse) {
+    expenses.push(expanse);
+}
+
+function getExpanseFromUser() {
+    if (!inputNode.value) {
+        return null;
+    }
+
     const expense = parseInt(inputNode.value);
 
-    //инпут на ноль
+    clearInput();
+
+    return expense;
+};
+
+function clearInput() {
     inputNode.value = '';
+};
 
-    //добавляем значение инпута в массив
-    expenses.push(expense);
-
-    //переменная для списка расходов
-    let expensionListHTML = '';
-
-    //цикл для добавления расхода в список
-    expenses.forEach(element => {
-        expensionListHTML += `<li>${element} руб.</li>`;
-    });
-
-    //записываем результат в html таблицу
-    historyNode.innerHTML = `<ol>${expensionListHTML}</ol>`;
-
-    //посчитать сумму
+function calculateExpanses(expenses) {
     let sum = 0;
 
     expenses.forEach(element => {
         sum += element;
     });
 
+    return sum;
+};
+
+function render(expenses){
+    const sum = calculateExpanses(expenses);
+
+    renderHistory(expenses);
+    renderSum(sum);
+    renderStatus(sum);
+};
+
+function renderHistory(expenses) {
+    let expensionListHTML = '';
+
+    expenses.forEach(element => {
+        expensionListHTML += `<li>${element} ${CURRENCY}</li>`;
+    });
+
+    historyNode.innerHTML = `<ol>${expensionListHTML}</ol>`;
+}
+
+function renderSum(sum) {
     sumNode.innerText = sum;
+};
 
-    //сравниваем сумму с лимитом м выводим статус
+function renderStatus(sum) {
     if (sum <= LIMIT) {
-        statusNode.innerText = 'all good';
-        statusNode.classList.add('status_good');
+        statusNode.innerText = STATUS_IN_LIMIT;
+        statusNode.classList.add(STATUS_IN_LIMIT_CLASSNAME);
     } else {
-        statusNode.innerText = 'all bad';
-        statusNode.classList.add('status_bad');
+        statusNode.innerText = STATUS_OUT_OF_LIMIT;
+        statusNode.classList.add(STATUS_OUT_OF_LIMIT_CLASSNAME);
     }
-
-    console.log(sum);
-})
+};
