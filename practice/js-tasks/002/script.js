128f46053bebb3db09590bfe0e42877e78c37308
Sergey Sergeev <giiizmo270286@gmail.com>
2023-05-15 00:03:55 +0700

    js

--- practice/js-tasks/002/script.js
+++ practice/js-tasks/002/script.js
@@ -5,7 +5,7 @@ const inputNode = document.getElementsByClassName('num');
 const func = () => {
     let sum = 0;
     for (let i = 0; i < inputNode.length; i++) {
-        sum += inputNode.value;
+        sum += inputNode[i].value;
     }
     const resultNode = document.getElementById('result');
     resultNode.innerHTML = sum;
