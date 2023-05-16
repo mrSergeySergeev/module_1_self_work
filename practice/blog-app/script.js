d16a5efd61da64bebb37bbb95b22291b235c175e
Sergey Sergeev <giiizmo270286@gmail.com>
2023-05-16 22:04:23 +0700

    blog-app

---
+++ practice/blog-app/script.js
@@ -0,0 +1,7 @@
+let postItem = '';
+
+const titleInputNode = document.getElementById('postTitleInput');
+const descriptionInputNode = document.getElementById('postDescriptionInput');
+const newPostButtonNode = document.getElementById('newPostButton');
+const postsNode = document.getElementById('');
+
