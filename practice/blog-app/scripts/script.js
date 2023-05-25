68acc9f7625a0432c8e2d7c1fd0e1ea0ded742dc
Sergey Sergeev <giiizmo270286@gmail.com>
2023-05-26 00:50:31 +0700

    blog-app

--- practice/blog-app/scripts/script.js
+++ practice/blog-app/scripts/script.js
@@ -1,58 +1,54 @@
+const ZERO = 0;
+const INIT_TITLE_VALIDATION_MESSAGE = 'введите заголовок';
+const TITLE_VALIDATION_MAX_SUM_SYMBOLS = 50;
+const INIT_TEXT_VALIDATION_MESSAGE = 'введите текст';
+const TEXT_VALIDATION_MAX_SUM_SYMBOLS = 200;
+
 const posts = [];
 
 const titleInputNode = document.getElementById('postTitleInput');
 const textInputNode = document.getElementById('postTextInput');
 const postsListNode = document.getElementById('postList');
-
 const newPostButtonNode = document.getElementById('newPostButton');
+const titleValidationNode = document.getElementById('titleValidation');
+const textValidationNode = document.getElementById('textValidation');
 
-const validationMessageNode = document.getElementsByClassName('popup__title')
+function checkTitle() {
+    const titleString = titleInputNode.value;
 
-checkTitle = () => {
-    const string = titleInputNode.value;
-    let check = string.split('');
-    if (check.length <= 0) {
-        console.log('errorCheckTitle');
+    let titleCheck = titleString.split('');
+
+    if (titleCheck.length <= ZERO) {
+        titleValidationNode.innerText = INIT_TITLE_VALIDATION_MESSAGE;
         return;
     };
-    if (check.length >= 100) {
-        console.log('errorCheckTitle');
+    if (titleCheck.length >= ZERO) {
+        titleValidationNode.innerText = `${INIT_TITLE_VALIDATION_MESSAGE}. осталось:${TITLE_VALIDATION_MAX_SUM_SYMBOLS - titleCheck.length} символов`;
         return;
     };
+
 };
 
-checkText = () => {
-    const string = textInputNode.value;
-    let check = string.split('');
-    if (check.length <= 0) {
-        console.log('errorCheckText');
+function checkText() {
+    const textString = textInputNode.value;
+    let textCheck = textString.split('');
+    if (textCheck.length <= ZERO) {
+        textValidationNode.innerText = INIT_TEXT_VALIDATION_MESSAGE;
         return;
     };
-    if (check.length >= 200) {
-        console.log('errorCheckText');
+    if (textCheck.length >= ZERO) {
+        textValidationNode.innerText = `${INIT_TEXT_VALIDATION_MESSAGE}. осталось:${TEXT_VALIDATION_MAX_SUM_SYMBOLS - textCheck.length} символов`;
         return;
     };
-};
-
+}
 
 initPostList = () => {
-    const currentTitle = checkTitle();
-    if (!currentTitle) {
-        console.log('errorInitTitle');
-        return;
-    }
-    const currentText = checkText();
-    if (!currentText) {
-        console.log('errorInitText');
-        initInputValue;
-        return;
-    }
+    const currentTitle = titleInputNode.value;
+    const currentText = textInputNode.value;
 
     let post = { title: currentTitle, text: currentText, }
 
-    posts.push(post);
-    textInputNode.value = "";
-    titleInputNode.value = "";    
+    posts.push(post);    
 };
 
 renderPostList = () => {
@@ -64,6 +60,21 @@ renderPostList = () => {
 
         postsListNode.appendChild(listItem);
     });
+};
+
+function initialValues() {
+    textInputNode.value = "";
+    titleInputNode.value = "";
+    titleValidationNode.innerText = INIT_TITLE_VALIDATION_MESSAGE;
+    textValidationNode.innerText = INIT_TEXT_VALIDATION_MESSAGE;
 }
 
-newPostButtonNode.addEventListener('click', renderPostList());
+function newPostHandler() {
+    initPostList();
+    renderPostList();
+    initialValues();
+};
+
+titleInputNode.addEventListener('input', checkTitle);
+textInputNode.addEventListener('input', checkText);
+newPostButtonNode.addEventListener('click', newPostHandler);
