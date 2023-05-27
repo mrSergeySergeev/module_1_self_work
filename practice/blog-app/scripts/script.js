cb2510f830b00c3a87447bd98cd060ab28ca2cc0
Sergey Sergeev <giiizmo270286@gmail.com>
2023-05-27 14:48:29 +0700

    blog-app

--- practice/blog-app/scripts/script.js
+++ practice/blog-app/scripts/script.js
@@ -14,27 +14,78 @@ const resetButtonNode = document.getElementById('resetButton');
 const titleValidationNode = document.getElementById('titleValidation');
 const textValidationNode = document.getElementById('textValidation');
 
+// функция опредения окончания строки заголовка
+function checkEndingStringTitle() {
+    let endingStringTitle = TITLE_VALIDATION_MAX_SUM_SYMBOLS - titleInputNode.value.length;
+    endingStringTitle = String(endingStringTitle);
+    endingStringTitle = endingStringTitle.split('');
+    endingStringTitle = endingStringTitle[endingStringTitle.length - 1];
+    if (endingStringTitle == 0) {
+        endingStringTitle = 'ов';
+        return endingStringTitle;
+    };
+    if (endingStringTitle == 1) {
+        endingStringTitle = '';
+        return endingStringTitle;
+    };
+    if (endingStringTitle > 1 && endingStringTitle <= 4) {
+        endingStringTitle = 'a';
+        return endingStringTitle;
+    }  
+    if (endingStringTitle > 4 && endingStringTitle <=9) {
+        endingStringTitle = 'ов';
+        return endingStringTitle;
+    };
+    return endingStringTitle;    
+}
+
+// функция опредения окончания строки текста поста
+function checkEndingStringText() {
+    let endingStringText = TEXT_VALIDATION_MAX_SUM_SYMBOLS - textInputNode.value.length;
+    endingStringText = String(endingStringText);
+    endingStringText = endingStringText.split('');
+    endingStringText = endingStringText[endingStringText.length - 1];
+    if (endingStringText == 0) {
+        endingStringText = 'ов';
+        return endingStringText;
+    };
+    if (endingStringText == 1) {
+        endingStringText = '';
+        return endingStringText;
+    };
+    if (endingStringText > 1 && endingStringText <= 4) {
+        endingStringText = 'a';
+        return endingStringText;
+    }  
+    if (endingStringText > 4 && endingStringText <=9) {
+        endingStringText = 'ов';
+        return endingStringText;
+    };
+    return endingStringText;    
+};
+
 // функция валидации заголовка поста
 function checkTitle() {
     const titleString = titleInputNode.value;
 
     let titleCheck = titleString.split('');
 
+    checkEndingStringTitle();
     if (titleCheck.length <= ZERO) {
         titleValidationNode.innerText = INIT_TITLE_VALIDATION_MESSAGE;
         return;
     };
 
     if (titleCheck.length > ZERO && titleCheck.length <= TITLE_VALIDATION_MAX_SUM_SYMBOLS) {
-        titleValidationNode.innerText = `Осталось ${TITLE_VALIDATION_MAX_SUM_SYMBOLS - titleCheck.length} символов`;
+        titleValidationNode.innerText = `Осталось ${TITLE_VALIDATION_MAX_SUM_SYMBOLS - titleCheck.length} символ${checkEndingStringTitle()}`;
         return;
     } else {
-        titleValidationNode.innerText = `Длина заголовка превышена${TITLE_VALIDATION_MAX_SUM_SYMBOLS - titleCheck.length} символов`;
+        titleValidationNode.innerText = `Длина заголовка превышена${TITLE_VALIDATION_MAX_SUM_SYMBOLS - titleCheck.length} символ${checkEndingStringTitle()}`;
     };
 };
 
 // функция валидации текста поста
-function checkText() {
+function checkText(endingStringText) {
     const textString = textInputNode.value;
 
     let textCheck = textString.split('');
@@ -45,10 +96,10 @@ function checkText() {
     };
 
     if (textCheck.length > ZERO && textCheck.length <= TEXT_VALIDATION_MAX_SUM_SYMBOLS) {
-        textValidationNode.innerText = `Осталось ${TEXT_VALIDATION_MAX_SUM_SYMBOLS - textCheck.length} символов`;
+        textValidationNode.innerText = `Осталось ${TEXT_VALIDATION_MAX_SUM_SYMBOLS - textCheck.length} символ${checkEndingStringText()}`;
         return;
     } else {
-        textValidationNode.innerText = `Длина текста поста превышена${TEXT_VALIDATION_MAX_SUM_SYMBOLS - textCheck.length} символов`;
+        textValidationNode.innerText = `Длина текста поста превышена${TEXT_VALIDATION_MAX_SUM_SYMBOLS - textCheck.length} символ${checkEndingStringText()}`;
     };
 };
 
