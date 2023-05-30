a62fc5c566fbf470072133df78a4a68216b67eca
Sergey Sergeev <giiizmo270286@gmail.com>
2023-05-30 15:06:49 +0700

    self-work

--- practice/self-work/script.js
+++ practice/self-work/script.js
@@ -7,15 +7,16 @@ const filmInputNode = document.getElementById('filmInput');
 const addFilmButtonNode = document.getElementById('addButton');
 const filmListNode = document.getElementById('filmList');
 const filmItemNode = document.getElementById('filmItem');
+const filmNameNode = document.getElementsByClassName('filmItem__name');
 const validationMessageNode = document.getElementById('validationMessage');
 
 // функция валидации заголовка
 function checkInput() {
     const lengthString = filmInputNode.value.length;
     if (lengthString > ZERO && lengthString <= MAX_SYMBOLS_IN_INPUT) {
-        validationMessageNode.innerText = `${MAX_SYMBOLS_IN_INPUT - filmInputNode.value.length}/50`
+        validationMessageNode.innerText = `${MAX_SYMBOLS_IN_INPUT - filmInputNode.value.length}/50`;
     } else {
-        validationMessageNode.innerText = `${MAX_SYMBOLS_IN_INPUT - filmInputNode.value.length}/50`
+        validationMessageNode.innerText = `${MAX_SYMBOLS_IN_INPUT - filmInputNode.value.length}/50`;
     };
 };
 
@@ -28,9 +29,24 @@ function checkButton() {
     };
 }
 
+function initFilmList() {
+    let filmName = filmNameNode;        
+    console.log(filmName[0]);
+    return;
+}
+
+
+
+
+
+
+
+
+
 function initInputHandler() {
     checkInput();
     checkButton();
 };
 
-filmInputNode.addEventListener('input', initInputHandler)
+filmInputNode.addEventListener('input', initInputHandler);
+addFilmButtonNode.addEventListener('click', initFilmList);
