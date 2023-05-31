05d1df38e6a040cf364c4da44d7e978850361c12
Sergey Sergeev <giiizmo270286@gmail.com>
2023-05-31 15:50:40 +0700

    1weekJS

--- practice/js-tasks/sd-code-tasks/script.js
+++ practice/js-tasks/sd-code-tasks/script.js
@@ -46,6 +46,7 @@ buttonTask3Node.addEventListener('click', isAdult);
 const buttonTask4Node = document.getElementById('task4');
 const promtMessage = 'Введите месяц(от 1 до 12)'
 const mounthArray = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь', 'ошибка...'];
+
 const getMounthByNumber = () => {
     let currentMounth = parseInt(prompt(promtMessage));
     switch (currentMounth) {
@@ -106,3 +107,38 @@ const getMounthByNumber = () => {
 };
 
 buttonTask4Node.addEventListener('click', getMounthByNumber);
+
+// task#5
+const buttonTask5Node = document.getElementById('task5');
+const sumMessage = 'Введите кол-во денюжек(число)';
+const moneyMessage = 'Введите валюту для конвертации';
+const notCorrect = 'не надо вводить некорректные значения';
+const moneyCurrency = ['USD', 'EUR', 'KZT', 'AED'];
+
+const convertRub = () => {
+    let valueRub = parseInt(prompt(sumMessage));
+    let currency = prompt(moneyMessage, moneyCurrency);
+    valueRub = valueRub * 100;
+    switch (currency) {
+        case moneyCurrency[0]:
+            valueRub = valueRub / 80.69;
+            break;
+        case moneyCurrency[1]:
+            valueRub = valueRub / 86.51;
+            break;
+        case moneyCurrency[2]:
+            valueRub = valueRub / 0.1815;
+            break;
+        case moneyCurrency[3]:
+            valueRub = valueRub / 21.8;
+            break;
+        default:
+            alert(notCorrect);
+            return;
+    };
+    valueRub = Math.round(valueRub);
+    valueRub = valueRub / 100;
+    alert(`информация актуальна нa 31.5.2023: ${valueRub}`);
+};
+
+buttonTask5Node.addEventListener('click', convertRub);
