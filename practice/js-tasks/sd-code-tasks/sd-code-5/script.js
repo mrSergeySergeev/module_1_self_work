a0f5eb53533a5cafe45889630003ff188cf9f517
Sergey Sergeev <giiizmo270286@gmail.com>
2023-06-02 15:54:28 +0700

    1weekJS

--- practice/js-tasks/sd-code-tasks/sd-code-5/script.js
+++ practice/js-tasks/sd-code-tasks/sd-code-5/script.js
@@ -13,58 +13,63 @@ const getValueFromUser = () => {
 };
 
 const getMoneyTypeFromUser = () => {
-    while (!true) {
+    for (let i = 0; i < 2; i++) {
         currency = prompt(moneyMessage, moneyCurrency);
-        checkCurrency(currency);
-        return;
+        switch (currency) {
+            case moneyCurrency[0]:
+                i = 1;
+                break;
+            case moneyCurrency[1]:
+                i = 1;
+                break;
+            case moneyCurrency[2]:
+                i = 1;
+                break;
+            case moneyCurrency[3]:
+                i = 1;
+                break;
+            default:
+                i = 0;
+                break;
+        };
     };
+
+    console.log(currency)
+    return currency;
 };
 
-const checkCurrency = () => {
-    let currency = getMoneyTypeFromUser();
-    for (let i = 0; i < moneyCurrency.length; i++) {
-        if (currency === moneyCurrency[i]) {
-            console.log(true);
-            return true;
-        }
-        console.log(false);
-        return false
-    };
-};
-
-
-const convertRub = () => {
-    let valueRub = getValueFromUser();
-    let currency = getMoneyTypeFromUser();
-    valueRub = valueRub * 100;
+const convertRub = (valueRub, currency) => {
+    let valueCurrency = null;
+    valueCurrency = valueRub * 100;
     switch (currency) {
         case moneyCurrency[0]:
-            valueRub = valueRub / 80.69;
+            valueCurrency = valueCurrency / 80.69;
             break;
         case moneyCurrency[1]:
-            valueRub = valueRub / 86.51;
+            valueCurrency = valueCurrency / 86.51;
             break;
         case moneyCurrency[2]:
-            valueRub = valueRub / 0.1815;
+            valueCurrency = valueCurrency / 0.1815;
             break;
         case moneyCurrency[3]:
-            valueRub = valueRub / 21.8;
+            valueCurrency = valueCurrency / 21.8;
             break;
     };
-    valueRub = Math.round(valueRub);
-    valueRub = valueRub / 100;
-    return valueRub;
+    valueCurrency = Math.round(valueCurrency);
+    valueCurrency = valueCurrency / 100;
+    return valueCurrency;
 };
 
-const render = (valueRub) => {
-    textNode.innerText = `${valueRub}`
+const render = (valueRub, currency, valueCurrency) => {
+    textNode.innerText = `${valueRub} RUB - это ${valueCurrency} ${currency}`
 };
 
 const buttonHandler = () => {
     console.log(moneyCurrency)
-    let valueRub = convertRub();
-    // let currency = getMoneyTypeFromUser();
-    render(valueRub);
+    let valueRub = getValueFromUser();
+    let currency = getMoneyTypeFromUser();
+    let valueCurrency = convertRub(valueRub, currency);
+    render(valueRub, currency, valueCurrency);
 };
 
 buttonTask5Node.addEventListener('click', buttonHandler);
