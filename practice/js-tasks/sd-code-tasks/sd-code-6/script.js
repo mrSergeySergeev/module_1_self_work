7de7d43d958baf8ac3ef8b78e8d612a095dd4d00
Sergey Sergeev <giiizmo270286@gmail.com>
2023-06-05 11:05:27 +0700

    1weekJS

--- practice/js-tasks/sd-code-tasks/sd-code-6/script.js
+++ practice/js-tasks/sd-code-tasks/sd-code-6/script.js
@@ -1,13 +1,15 @@
 const buttonNode = document.getElementById('button');
 const textNode = document.getElementById('text');
 
-const getNumberMessage = 'введите число';
+const getNumberMessage = 'введите число от -10 до 10';
 
 const getNumberFromUser = () => {
-    let a;
-    a = parseInt(prompt(getNumberMessage));
+    let a = null;
+    while (a < -10 || a > 10 || a == null) {
+        a = parseInt(prompt(getNumberMessage));
+    };
     return a;
-}
+};
 
 const formArray = () => {
     let arr = [];
@@ -22,15 +24,12 @@ const formArray = () => {
     for (let i = b; i <= a; i++) {
         arr.push(i);
     };
-    console.log(arr);
     return arr;
 };
 
 const renderArray = (arr) => {
-    for (let i = 0; i < arr.length; i++) {
-        textNode.innerText = arr[i];
-    };    
+    arr = formArray();
+    textNode.innerText = arr.join(' ');
 };
 
-
-buttonNode.addEventListener('click', formArray);
+buttonNode.addEventListener('click', renderArray);
