9d47ed271ecdbf4e51944c284e600bb53aa22d63
Sergey Sergeev <giiizmo270286@gmail.com>
2023-06-25 11:55:20 +0700

    selfwork

--- practice/try-again-self-work/script.js
+++ practice/try-again-self-work/script.js
@@ -14,6 +14,7 @@ const filmEmptyItemNode = document.querySelector('#filmEmptyItem');
 // функция валидации заголовка
 const checkInput = () => {
     const lengthString = filmInputNode.value.length;
+
     if (lengthString > INIT_SUM_SYMBOLS && lengthString <= MAX_SYMBOLS_IN_INPUT) {
         validationMessageNode.innerText = `${MAX_SYMBOLS_IN_INPUT - filmInputNode.value.length}/50`;
     } else {
@@ -24,6 +25,7 @@ const checkInput = () => {
 // чекаем кнопку по длине строки инпута
 const checkButton = () => {
     const lengthString = filmInputNode.value.length;
+    
     if (lengthString > INIT_SUM_SYMBOLS && lengthString <= MAX_SYMBOLS_IN_INPUT) {
         addFilmButtonNode.disabled = false;
     } else {
@@ -38,6 +40,13 @@ const checkEmptyFilmList = () => {
     };
 };
 
+// check dliny spiska films
+const makeIdForCheckbox = () => {
+    const lengthList = filmListNode.children.length;
+    const idItem = parseInt(lengthList);
+    console.log(lengthList);
+    return idItem;
+}
 
 // возвращаем начальные значения для ввода и кнопки
 const resetInputAndButton = () => {
@@ -48,24 +57,50 @@ const resetInputAndButton = () => {
     filmInputNode.focus();
 };
 
-const addFilm = (event) => {
-    // эта строчка отменяет стандартное поведение браузера 
-    //при отправке формы, здесь перезагрузку браузера
-    event.preventDefault();
-    const film = filmInputNode.value;
+// добавить фильм
+const addFilm = (idItem) => {
+    idItem = makeIdForCheckbox();
+    const film = filmInputNode.value;    
     const filmHtml = `
     <li id="filmItem" class="filmItem">
         <div class="filmItem__leftWrapper">
-            <input id="checkFilm0" class="filmItem__check" type="checkbox">
-            <label for="checkFilm0"></label>
+            <input id="checkFilm${idItem}" data-action="done" class="filmItem__check" type="checkbox">
+            <label for="checkFilm${idItem}"></label>
             <p id="filmName" class="filmItem__name">${film}</p>
         </div>
-        <button id="filmDelete0" class="filmItem__delete"><img src="resources/button-close.png" alt="X"></button>
+        <button id="filmDelete" data-action="delete" class="filmItem__delete"><img src="resources/button-close.png" alt="X"></button>
     </li>`;
     filmListNode.insertAdjacentHTML('beforeend', filmHtml);
-    resetInputAndButton();
-    checkEmptyFilmList();
 };
+
+// удалить фильм
+const deleteFilm = (event) => {
+    if (event.target.dataset.action !== 'delete') {
+        return;
+    };
+
+    const currentParentNode = event.target.closest('.filmItem');
+    currentParentNode.remove();
+
+    if (filmListNode.children.length === 1) {
+        filmEmptyItemNode.classList.remove('none')
+    };
+};
+
+// otmetit' prosmotrennim
+const doneFilm = (event) => {
+    if (event.target.dataset.action !== 'done') {
+        return;
+    };
+
+    const currentParentNode = event.target.closest('.filmItem');
+
+    if (event.target.checked) {
+        currentParentNode.classList.add('filmItem__name-disabled');
+    } else {
+        currentParentNode.classList.remove('filmItem__name-disabled');
+    };
+}
 
 // обработчик поля ввода фильма
 const initInputHandler = () => {
@@ -73,8 +108,24 @@ const initInputHandler = () => {
     checkButton();
 };
 
+// obrabot4ik knopki addfilm
+const addFilmHandler = (event) => {
+    // эта строчка отменяет стандартное поведение браузера 
+    // при отправке формы, здесь перезагрузку браузера
+    event.preventDefault();
+    addFilm();
+    resetInputAndButton();
+    checkEmptyFilmList();
+};
+
 // слушатель поля ввода
 filmInputNode.addEventListener('input', initInputHandler);
 
 // слушатель отправки формы
-filmFormNode.addEventListener('submit', addFilm);
+filmFormNode.addEventListener('submit', addFilmHandler);
+
+// slyshatel' ydalit' film
+filmListNode.addEventListener('click', deleteFilm);
+
+// slyshatel' "done" film
+filmListNode.addEventListener('click', doneFilm); 
